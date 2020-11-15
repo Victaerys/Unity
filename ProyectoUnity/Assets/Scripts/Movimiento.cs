b4cb86c184e8339e1c8f132a6d8d3efdd1603b55
Víctor <victorclaramuntv@gmail.com>
2020-11-15 23:57:13 +0100

    Build añadido. Animaciones de salto y caída implementadas correctamente.

--- ProyectoUnity/Assets/Scripts/Movimiento.cs
+++ ProyectoUnity/Assets/Scripts/Movimiento.cs
@@ -8,11 +8,14 @@ public class Movimiento : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
     public SpriteRenderer sr;
+    public Transform trans;
 
     public float velocidad = 5f;
     public float sprint = 5f;
-    public float fuerzaSalto = 5f;
+    public float fuerzaSalto = 6.5f;
     private bool tocaSuelo = false;
+    private float ejeYAnterior;
+    private float ejeY;
 
     // Start is called before the first frame update
     void Start()
@@ -20,19 +23,24 @@ public class Movimiento : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
+        trans = GetComponent<Transform>();
         tocaSuelo = false;
+        ejeYAnterior = trans.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        //Correr
+
         float movimiento = Input.GetAxis("Horizontal");
         if (movimiento != 0) {
 
             animator.SetBool("estaCorriendo", true);
             sr.flipX = movimiento < 0;
 
-            if (Input.GetKey(KeyCode.LeftShift))
+            if (Input.GetKey(KeyCode.LeftShift) && tocaSuelo == true)
                 rb.velocity = new Vector2((velocidad + sprint) * movimiento, rb.velocity.y);
             else
                 rb.velocity = new Vector2(velocidad * movimiento, rb.velocity.y);
@@ -40,18 +48,33 @@ public class Movimiento : MonoBehaviour
         }
         else animator.SetBool("estaCorriendo", false);
 
+
+        //Saltar y caer
+
         if (Input.GetKey(KeyCode.Space) && tocaSuelo==true)
         {
-            
+            animator.SetBool("estaSaltando", true);
             rb.velocity = new Vector2(rb.velocity.x, fuerzaSalto);
         }
-  
-}
+
+        ejeY = trans.position.y;
+
+        if (ejeY > ejeYAnterior && tocaSuelo == false)
+            animator.SetBool("estaSaltando", true);
+        else if (ejeY < ejeYAnterior)
+        {
+            animator.SetBool("estaSaltando", false);
+            animator.SetBool("estaCayendo", true);
+        } 
+
+        ejeYAnterior = ejeY;
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Plataforma")
         {
             tocaSuelo = true;
+            animator.SetBool("estaCayendo", false);
             animator.SetBool("estaSaltando", false);
         }
     }
@@ -61,7 +84,6 @@ public class Movimiento : MonoBehaviour
         if (collision.gameObject.tag == "Plataforma")
         {
             tocaSuelo = false;
-            animator.SetBool("estaSaltando", true);
         }
     }
 
