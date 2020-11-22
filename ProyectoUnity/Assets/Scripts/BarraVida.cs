2d79d514004b32d5bc0388e4a51a5d5d2cd42711
Víctor <victorclaramuntv@gmail.com>
2020-11-22 01:43:30 +0100

    Añadida barra de vida no funcional.

---
+++ ProyectoUnity/Assets/Scripts/BarraVida.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarraVida : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void setMaxVida(int vida)
+    {
+        slider.maxValue = vida;
+        slider.value = vida;
+    }
+
+   public void setVida(int vida)
+    {
+        slider.value = vida;
+    }
+
+    
+}
---
+++ ProyectoUnity/Assets/Scripts/vidaJugador.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class vidaJugador : MonoBehaviour
+{
+
+    public int maxVida = 100;
+    public int vidaActual;
+
+    public BarraVida barraVida;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidaActual = maxVida;
+        barraVida.setMaxVida(maxVida);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void dañoSufrido(int daño)
+    {
+        vidaActual -= daño;
+        barraVida.setVida(vidaActual);
+
+    }
+}
